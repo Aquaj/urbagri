7d52c3b9e40252c83b0cafa35e312d2222d4b931
Jeremie Bonal <jeremie.bonal@gmail.com>
2016-10-18 12:27:55 +0200

    Locale issue fixed.

--- app/controllers/selling_points_controller.rb
+++ app/controllers/selling_points_controller.rb
@@ -1,5 +1,5 @@
 class SellingPointsController < ApplicationController
   def index
-    i18n.locale = params[:locale] if I18n.available_locales.include? params[:locale]
+    I18n.locale = params[:locale] if I18n.available_locales.include? params[:locale].to_sym
   end
 end
